f956f21175bc7eb8bfe94a8157c6aa622e12ea71
Bhavesh Dewangan <ybkdddd@gmail.com>
2022-09-10 23:07:35 +0530

    get users through database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import logo from "./logo.svg";
+import { collection, getDoc, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { db } from "./firebase-config";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const userCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(userCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data.docs);
+      //console.log(data);
+    };
+
+    getUsers();
+  }, []);
   return <div className="App"></div>;
 }
 
